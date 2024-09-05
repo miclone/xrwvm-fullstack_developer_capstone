29f4140f3338f9c420c3cddf0f377a08f6e13803
Michael Tan <miclone.research@gmail.com>
2024-09-05 21:53:21 +1000

    Update views.py
    fixed

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -45,14 +45,14 @@ def logout_user(request):
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
-def registration(request):    
+def registration(request):
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
-    username_exist = False    
+    username_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -73,7 +73,7 @@ def registration(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
@@ -82,7 +82,7 @@ def registration(request):
 # a list of dealerships
 # def get_dealerships(request):
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/" + state
@@ -120,13 +120,13 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
-        try:
-            response = post_review(data)
+        try:            
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401,"message": "Error in posting review"})
+            return JsonResponse({"status": 401, "message": 
+                                 "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
@@ -139,5 +139,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, 
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
