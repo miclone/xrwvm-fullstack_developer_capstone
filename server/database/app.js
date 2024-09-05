678b2a52f03d6362c70caea060111ccd4ab83ccd
Michael Tan <miclone.research@gmail.com>
2024-09-05 22:35:51 +1000

    Update app.js
    fixed

--- server/database/app.js
+++ server/database/app.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 6 */
 /*jshint esversion: 8 */
 
 const express = require('express');
@@ -23,10 +22,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
