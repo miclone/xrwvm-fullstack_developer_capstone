6ab38288a1400883f497832f7dbed324c66fe1aa
Michael Tan <miclone.research@gmail.com>
2024-09-05 21:02:19 +1000

    Update models.py
    make line shorter

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -30,8 +30,10 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
